4d5c336c5faae467f8a680ce868fb3a2f4590317
Leonardo Ferreira Guimarães <lonardow_fg@hotmail.com>
2023-04-01 21:08:56 -0300

    up

--- TodoListApp/TodoList/settings.py
+++ TodoListApp/TodoList/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-%+$a=i&w-_(_oc_fwn4z69cjkyc=e4613lcbiel-uwtdn^o)g+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,12 +119,10 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATIC_ROOT = 'staticfiles/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    'static'
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
